4cb13524754a3996b334728e1b543ab65167b27d
Stephano Balbinot <stephanobalbinot@hotmail.com>
2021-10-25 18:48:34 -0300

    feat: ensure FacebookAuthenticationService calls CreateFacebookAccountRepository when LoadUserAccountRepository returns undefined

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,18 +1,20 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
-import { LoadUserAccountRepository } from '@/data/contracts/repositories'
+import { CreateFacebookAccountRepository, LoadUserAccountRepository } from '@/data/contracts/repositories'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 
 export class FacebookAuthenticationService {
   constructor (
     private readonly loadFacebookUserApi: LoadFacebookUserApi,
-    private readonly loadUserAccountRepository: LoadUserAccountRepository
+    private readonly loadUserAccountRepository: LoadUserAccountRepository,
+    private readonly createFacebookAccountRepository: CreateFacebookAccountRepository
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const facebookData = await this.loadFacebookUserApi.loadUser(params)
     if (facebookData !== undefined) {
       await this.loadUserAccountRepository.load({ email: facebookData.email })
+      await this.createFacebookAccountRepository.createFromFacebook(facebookData)
     }
     return new AuthenticationError()
   }
