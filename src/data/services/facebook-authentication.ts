7f5e712a34fa8ab64f7f600a3bc47f5bd242e5bc
Stephano Balbinot <stephanobalbinot@hotmail.com>
2021-10-26 20:10:04 -0300

    feat: ensure FacebookAuthenticationService calls TokenGenerator with correct params

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -1,4 +1,5 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
+import { TokenGenerator } from '@/data/contracts/crypto'
 import { LoadUserAccountRepository, SaveFacebookAccountRepository } from '@/data/contracts/repositories'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
@@ -7,7 +8,8 @@ import { FacebookAccount } from '@/domain/models'
 export class FacebookAuthenticationService {
   constructor (
     private readonly facebookApi: LoadFacebookUserApi,
-    private readonly userAccountRepository: LoadUserAccountRepository & SaveFacebookAccountRepository
+    private readonly userAccountRepository: LoadUserAccountRepository & SaveFacebookAccountRepository,
+    private readonly crypto: TokenGenerator
   ) {}
 
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
@@ -15,7 +17,8 @@ export class FacebookAuthenticationService {
     if (facebookData !== undefined) {
       const accountData = await this.userAccountRepository.load({ email: facebookData.email })
       const facebookAccount = new FacebookAccount(facebookData, accountData)
-      await this.userAccountRepository.saveWithFacebook(facebookAccount)
+      const { id } = await this.userAccountRepository.saveWithFacebook(facebookAccount)
+      await this.crypto.generateToken({ key: id })
     }
     return new AuthenticationError()
   }
