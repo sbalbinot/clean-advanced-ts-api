1b30c8bbbc7c8eb74d6a857271c02c630519b1c5
Stephano Balbinot <stephanobalbinot@hotmail.com>
2021-10-31 19:40:21 -0300

    feat: ensure FacebookApi returns facebook user

--- src/infra/apis/facebook.ts
+++ src/infra/apis/facebook.ts
@@ -1,10 +1,10 @@
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { HttpGetClient } from '@/infra/http'
 
-export class FacebookApi {
+export class FacebookApi implements LoadFacebookUserApi {
   private readonly baseUrl = 'https://graph.facebook.com'
   constructor (private readonly httpClient: HttpGetClient, private readonly clientId: string, private readonly clientSecret: string) {}
-  async loadUser (params: LoadFacebookUserApi.Params): Promise<void> {
+  async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
     const appToken = await this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
@@ -20,7 +20,7 @@ export class FacebookApi {
         input_token: params.token
       }
     })
-    await this.httpClient.get({
+    const userInfo = await this.httpClient.get({
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
       url: `${this.baseUrl}/${debugToken.data.user_id}`,
       params: {
@@ -28,5 +28,10 @@ export class FacebookApi {
         access_token: params.token
       }
     })
+    return {
+      facebookId: userInfo.id,
+      name: userInfo.name,
+      email: userInfo.email
+    }
   }
 }
