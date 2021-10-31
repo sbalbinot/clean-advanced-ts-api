3aa638688bc4d76caa766593bfe1d5c2195be48a
Stephano Balbinot <stephanobalbinot@hotmail.com>
2021-10-31 20:25:31 -0300

    feat: add axios integration

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -1,15 +1,8 @@
-import { HttpGetClient } from '@/infra/http'
+import { AxiosHttpClient } from '@/infra/http'
 
 import axios from 'axios'
 
 jest.mock('axios')
-
-class AxiosHttpClient {
-  async get (params: HttpGetClient.Params): Promise<any> {
-    const result = await axios.get(params.url, { params: params.params })
-    return result.data
-  }
-}
 
 describe('AxiosHttpClient', () => {
   let sut: AxiosHttpClient
